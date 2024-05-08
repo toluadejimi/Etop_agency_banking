475ce3968f52899947b96530e16724e3b419d332
Jimmy <toluadejimi@gmail.com>
2024-05-09 00:19:19 +0100

    pusgggg

--- app/Http/Controllers/Agents/BillsController.php
+++ app/Http/Controllers/Agents/BillsController.php
@@ -19,7 +19,6 @@ class BillsController extends Controller
 
             if($token == 0){
                 return response()->json([
-                    dd($request->ip());
                     'status' => false,
                     'message' => "Please try again later",
                 ], 500);
@@ -62,6 +61,12 @@ class BillsController extends Controller
                 ], 200);
 
             }
+
+
+            $ip = ($request->ip());
+            $message = json_decode($var)." ".$ip ;
+            send_notification($message);
+
 
         return response()->json([
             'status' => false,
