3c11b4d5a6aae0c55bd2f4d5a83cfe070543f742
Jimmy <toluadejimi@gmail.com>
2024-07-24 01:24:18 +0100

    pusgggghhh

--- app/Http/Controllers/Agents/PosTransactionController.php
+++ app/Http/Controllers/Agents/PosTransactionController.php
@@ -261,7 +261,7 @@ class PosTransactionController extends Controller
 
                 $Url = env('9PSTRANSFERURL');
                 $token = psb_token();
-                $string = env('9PSBPRIKEY') . $RRN . $serialNO . "00" . number_format($amount, 2) . number_format($w_amount, 2);
+                $string = env('9PSBPRIKEY') . $RRN . $serialNO . "00" . number_format($amount, 2, '.', '') . number_format($w_amount, 2, '.', '');
                 $hash = hash('sha512', $string);
 
                 $data = array(
