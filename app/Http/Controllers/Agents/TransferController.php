96a324cece1546c3a3636b99751322b515d21d10
Jimmy <toluadejimi@gmail.com>
2024-08-13 06:12:37 +0100

    pusgggghhh

--- app/Http/Controllers/Agents/TransferController.php
+++ app/Http/Controllers/Agents/TransferController.php
@@ -30,7 +30,8 @@ class TransferController extends Controller
         $startOfMonth = Carbon::now()->startOfMonth();
         $endOfMonth = Carbon::now()->endOfMonth();
         //$data['total_profit'] = Transaction::where('status', 2)->whereBetween('created_at', [$startOfMonth, $endOfMonth])->sum('charge');
-        $data['total_trx'] = Profit::latest()->paginate(10);
+        $data['profit'] = Profit::latest()->paginate(10);
+        $data['total_trx'] = Profit::where('status', 2)->sum('amount');
         $data['total_profit'] = Transaction::where('status', 2)->sum('etop_charge')  ?? 0;
 
         return view('profit', $data);
